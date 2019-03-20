ed4a9fe332d3569fb87bc792fd97dd200c83496e
Gustavo Sales <gustavosalessantos@gmail.com>
2019-03-19 22:52:01 -0300

    store and libs tests and coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -16,5 +16,13 @@ module.exports = {
 		'server/www.js',
 		'pages'
 	],
-	collectCoverage: true
+	collectCoverage: true,
+	coverageThreshold: {
+		global: {
+			branches: 100,
+			functions: 100,
+			lines: 100,
+			statements: 100
+		}
+	}
 };
